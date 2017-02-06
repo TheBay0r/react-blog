d4443c960a60b257b73a3c0030fba9ed480910cd
Stefan Schacherl <stefan.schacherl@kununu.com>
2017-02-06 13:01:28 +0100

    Refactor content to be loaded from static .js files

--- src/content/Content.js
+++ src/content/Content.js
@@ -2,14 +2,26 @@ import React from 'react';
 import BlogEntry from './blog/blog-entry/Blog-Entry';
 import About from './about/About';
 import './Content.css';
+import BlogEntriesContent from '../statics/blog-entries';
 
 var Content = React.createClass({
+  showForEachBlogEntry: function () {
+    let entriesList = BlogEntriesContent();
+
+    return entriesList.map(entry => (
+      <BlogEntry
+        key={entry.id}
+        timestamp={entry.timestamp}
+        content={entry.content}
+      />
+    ));
+  },
+
   showContent: function () {
     switch (this.props.currentTab) {
       case 1:
         return (<div>
-          <BlogEntry/>
-          <BlogEntry/>
+          {this.showForEachBlogEntry()}
         </div>);
       case 2:
         return (<About/>);
--- src/content/blog/blog-entry/Blog-Entry.js
+++ src/content/blog/blog-entry/Blog-Entry.js
@@ -2,11 +2,9 @@ import React, {Component} from 'react';
 import './Blog-Entry.css';
 
 class BlogEntry extends Component {
-  getBlogEntry() {
-    return `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`;
+
+  toUTCString(timestamp) {
+    return new Date(timestamp).toUTCString();
   }
 
   render() {
@@ -14,12 +12,12 @@ class BlogEntry extends Component {
       <div className="blog-entry">
         <div className="blog-date-container">
           <div className="blog-date">
-            Saturday, 14. January 2017
+            {this.toUTCString(this.props.timestamp)}
           </div>
           <div className="spacer"/>
         </div>
         <div className="blog-content">
-          {this.getBlogEntry()}
+          {this.props.content}
         </div>
       </div>
     );
